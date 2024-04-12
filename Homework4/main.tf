7fc3b8edabb59c143a5d60cc48a75661293cb833
Aijan13 <159641524+Aijan13@users.noreply.github.com>
2024-04-11 21:18:19 -0500

    Update main.tf

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -4,7 +4,7 @@ provider aws {
 
 
 resource "aws_key_pair" "my_key" {
-  key_name   = var.key_name
+  key_name   = var.key
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
@@ -30,13 +30,13 @@ resource aws_security_group "my_sec_group" {
 
 
 resource "aws_instance" "web" {
-  ami = var.ami_id
-  instance_type = var.type
-  count = var.count_number
+  ami                    = var.ami_id
+  instance_type          = var.type
+  count                  = var.count_number
   vpc_security_group_ids = [aws_security_group.my_sec_group.id]
-  availability_zone = var.availability_zone
-  key_name = aws_key_pair.my_key.key_name
-  security_groups = [aws_security_group.my_sec_group.name]
+  availability_zone      = var.availability_zone
+  key_name               = var.key
+  security_groups        = [aws_security_group.my_sec_group.name]
   associate_public_ip_address = true
   tags = {
     Name = "happy-${count.index + 1}"
