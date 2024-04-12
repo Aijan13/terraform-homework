a617c09bdd9eb3ed5c1acf62bdccffd0073583a0
Aijan13 <159641524+Aijan13@users.noreply.github.com>
2024-04-11 21:36:57 -0500

    Update main.tf

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -13,7 +13,23 @@ resource aws_security_group "my_sec_group" {
   description = "Allow TLS inbound traffic"
 
   ingress {
+    description      = "provide port 22"
     from_port        = var.ports[0]
+    to_port          = var.ports[0]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ ingress {
+    description      = "provide port 80"
+    from_port        = var.ports[1]
+    to_port          = var.ports[1]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    description      = "provide port 443"
+    from_port        = var.ports[2]
     to_port          = var.ports[2]
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
