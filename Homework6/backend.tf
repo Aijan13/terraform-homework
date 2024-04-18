5222b5a1718a47f2c371612df3302e45e533ecba
Aijan13 <159641524+Aijan13@users.noreply.github.com>
2024-04-18 02:20:36 -0500

    Update backend.tf

--- Homework6/backend.tf
+++ Homework6/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aijan-bucket"
-    key    = "ohio/terraform.tfstate"
+    key    = "state_file/terraform.tfstate"
     region = "us-east-2"
     dynamodb_table = "lock-state"
   }
